d3b03e48dc2451c6cac7ad579ff2bf19ebc4e1b4
leprosus <korolyov.denis@gmail.com>
2024-03-09 17:30:42 +0200

    Add: salt generator

---
+++ pkg/salt/salt.go
@@ -0,0 +1,15 @@
+package salt
+
+import (
+	"crypto/rand"
+	"io"
+)
+
+type Salt []byte
+
+func GenerateSalt(saltSize int) (s Salt, err error) {
+	s = make([]byte, saltSize)
+	_, err = io.ReadFull(rand.Reader, s)
+
+	return
+}
