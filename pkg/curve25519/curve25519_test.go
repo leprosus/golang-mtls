ca5e6bd78e6e3502818b73b7de24d62c4d246a78
leprosus <korolyov.denis@gmail.com>
2024-03-09 17:32:31 +0200

    Add: unit-test for curve25519

---
+++ pkg/curve25519/curve25519_test.go
@@ -0,0 +1,48 @@
+package curve25519
+
+import (
+	"bytes"
+	. "crypto/ed25519"
+	"mtls/pkg/ed25519"
+	"testing"
+)
+
+func TestGenerateSharedKey(t *testing.T) {
+	var (
+		alicePub  PublicKey
+		alicePriv PrivateKey
+		err       error
+	)
+
+	alicePub, alicePriv, err = ed25519.GenerateKeyPair()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	var (
+		bobPub  PublicKey
+		bobPriv PrivateKey
+	)
+	bobPub, bobPriv, err = ed25519.GenerateKeyPair()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	var (
+		aliceShared []byte
+		bobShared   []byte
+	)
+	aliceShared, err = GenerateSharedKey(bobPub, alicePriv)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	bobShared, err = GenerateSharedKey(alicePub, bobPriv)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if !bytes.Equal(aliceShared, bobShared) {
+		t.Fatal("got to different shared keys")
+	}
+}
