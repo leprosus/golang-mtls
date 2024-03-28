073bf7a8cb388365c579b5f3b83aa266ef61cdd0
leprosus <korolyov.denis@gmail.com>
2024-03-28 21:08:14 +0200

    Add: func (shared SharedKey) Sign()

--- pkg/ed25519/domain/shared_key.go
+++ pkg/ed25519/domain/shared_key.go
@@ -1,3 +1,30 @@
 package domain
 
+import (
+	"crypto/sha512"
+	"encoding/hex"
+	"strings"
+)
+
+const SignLength = 6
+
 type SharedKey []byte
+
+func (shared SharedKey) Sign() (sign string, err error) {
+	hashFn := sha512.New()
+	hashFn.Write(shared)
+	hash := hashFn.Sum(nil)
+	hashStr := hex.EncodeToString(hash)
+
+	hashLen := len(hashStr)
+	if hashLen < SignLength {
+		diff := SignLength - hashLen
+		padding := strings.Repeat("0", diff)
+
+		sign = padding + hashStr
+	} else {
+		sign = hashStr[0:SignLength]
+	}
+
+	return sign, nil
+}
