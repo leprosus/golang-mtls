3a65b9e91e93b1e5aec55d41d893839fb4b88722
leprosus <korolyov.denis@gmail.com>
2024-03-30 14:03:13 +0200

    Add: salt package unit-test

---
+++ pkg/salt/salt_test.go
@@ -0,0 +1,22 @@
+package salt_test
+
+import (
+	"testing"
+
+	"mtls/pkg/salt"
+)
+
+func TestGenerateSalt(t *testing.T) {
+	t.Parallel()
+
+	const length = 8
+
+	salt, err := salt.GenerateSalt(length)
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+
+	if len(salt) != length {
+		t.Fatal("GenerateSalt returns a unexpected length salt")
+	}
+}
